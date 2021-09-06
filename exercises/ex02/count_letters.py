081f6bb6ee7b7281f544ee411114f4279dadfcb0
Ananya Garg <akgarg@unc.edu>
2021-09-06 17:08:47 -0400

    progress on ex02

--- exercises/ex02/count_letters.py
+++ exercises/ex02/count_letters.py
@@ -5,14 +5,11 @@ __author__ = "ENTER YOUR 9-DIGIT PID HERE"
 searchLetter: str = input("What letter do you want to search for: ")
 word: str = input("Enter a word: ")
 i: int = 0
-occurrenceCount: int = 0
-lengthWord: int = len(word)
-while i < lengthWord:
-    if searchLetter in word:
-        occurrenceCount += 1
-    i += 1
-    word = word[i:lengthWord]
-if occurrenceCount > 0:
-    print("Count: " + str(occurrenceCount))
-else:
-    print("Count: 0")
+occurrence: int = 0
+while occurrence < len(word):
+    if searchLetter == word[occurrence]:
+        i += 1
+        occurrence += 1
+    else:
+        occurrence += 1
+print("Count: " + str(i))
