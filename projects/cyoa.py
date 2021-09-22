465eadb077ff583a5bd870e60daddcd2fb953cd2
Ananya Garg <akgarg@unc.edu>
2021-09-22 19:59:57 -0400

    progress

--- projects/cyoa.py
+++ projects/cyoa.py
@@ -3,11 +3,12 @@
 __author__ = "730475029"  
 import random
 points: int = 0
-player: str = input("What is your name? ")
+player: str
 WINKING_FACE: str = "\U0001F601"
 
 
 def main() -> None:
+    """Initializes program."""
     global points
     greet()
     while True:
@@ -15,18 +16,21 @@ def main() -> None:
         if decision == 1:
             name_game()
         if decision == 2:
-            number_game() 
+            points = number_game(points) 
         if decision == 3:
             print("Thanks for playing, see you next time!")
 
 
 def greet() -> None:
-    print("Welcome to the game!")
+    """Greets the player."""
     global player
+    print("Welcome to the game!")
+    player = input("What is your name? ")
+    return None
 
 
-def name_game():
-    greet()
+def name_game() -> None:
+    """Gives points for entering your age. Your age becomes the points you receive."""
     global points
     while points < 100: 
         age: int = int(input("What is your age? "))
@@ -37,9 +41,8 @@ def name_game():
         print("You have more than 100 points, congrats! Choose a new game play or play this game again!")
     
 
-def number_game():
-    greet()
-    global points
+def number_game(sum_points: int) -> int:
+    """A simple number guessing game."""
     global player
     print(f"Hey, {player}, welcome to the number guessing game. You have 10 tries to guess as many numbers as you can between 1-10. Every time you guess correctly, you get a point!")
     num: int = random.randint(1, 10)
@@ -53,8 +56,9 @@ def number_game():
             print("No that guess is too low, try again. ")
         if guess == num:
             print(f"Correct! Good job. The number was {num} and you got it in {tries} tries.")
-            points += 1
-    print(f"You finished the game with {points} points. Good job!")
+            sum_points += 1
+    print(f"You finished the game with {sum_points} points. Good job!")
+    return sum_points
 
 
 if __name__ == "__main__":
